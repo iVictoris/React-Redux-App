936dd83b1290ceab26684c14c3f5bdd50c920fd2
Victor Tran <victor.n.tran@outlook.com>
2019-12-01 10:16:52 -0500

    install redux thunk and use applyMiddleware

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,5 @@
-import {createStore} from 'redux';
+import thunk from 'redux-thunk'
+import {createStore, applyMiddleware} from 'redux';
 import {reducer} from '../reducers';
 
-export const store = createStore(reducer)
+export const store = createStore(reducer, applyMiddleware(thunk))
