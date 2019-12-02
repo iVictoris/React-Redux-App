49e8c58ee01561a15810d7ee6bc8334e90bd2722
Victor Tran <victor.n.tran@outlook.com>
2019-12-02 12:35:24 -0500

    create reducer with helper function to swap out activities

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,63 @@
-export const reducer = (state = {}, action) => {
-  switch(action.type) {
-    default: 
-    return state;
+import { state } from "../state";
+
+import {
+  FETCH_API_FAIL,
+  FETCH_API_START,
+  FETCH_API_SUCCESS
+} from "../actions/index";
+
+const replaceCurrentActivity = (
+  state = { current: null, previous: [] },
+  nextState = { nextCurrent: null, err: "" }
+) => {
+  const { previous, current } = state;
+  const { nextActivity, err } = nextState;
+  // if curent isn't null, add to previous array
+  const newPrevious = [...previous];
+  if (current) newPrevious.push(current);
+  const newState = {
+    ...state,
+    previous: newPrevious,
+    current: nextActivity,
+    err,
+    isFetching: false
+  };
+
+  return newState;
+};
+
+export const reducer = (initialState = state, action) => {
+  let newState;
+  switch (action.type) {
+    // fetch reducer
+    case FETCH_API_START:
+      console.log("Fetching start");
+      newState = { ...initialState, isFetching: true, err: "" };
+      return newState;
+
+    case FETCH_API_SUCCESS:
+      // get previousCurrent and place into previous
+      const newStateActivities = replaceCurrentActivity(
+        initialState,
+        {nextActivity: action.payload}
+      );
+      newState = {
+        ...newStateActivities
+      };
+      return newState;
+
+    case FETCH_API_FAIL:
+      const newErrorState = replaceCurrentActivity(initialState, {
+        err: action.payload
+      });
+      newState = {
+        ...newErrorState
+      };
+      return newState;
+
+    // end fetch reducer
+
+    default:
+      return initialState;
   }
-}
+};
