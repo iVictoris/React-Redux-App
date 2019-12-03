0ddfad46138661ac75c4662242f1d89c5e528042
Victor Tran <victor.n.tran@outlook.com>
2019-12-03 11:08:43 -0500

    add conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from "react";
 import "./App.css";
 import { connect } from "react-redux";
 import { getActivity } from "./redux/actions/creators";
-import ActivityCard from "./components/Activity/ActivityCard";
+import CurrentActivityCard from "./components/Activity/CurrentActivityCard";
+import PreviousActivities from './components/Activity/PreviousActivities';
 
 import './scss/index.scss';
 
 // boredapi.com
 const App = props => {
-  const {current} = props;
+  const {current, previous} = props;
   return (
     <div className="App">
       <button
@@ -20,13 +21,14 @@ const App = props => {
       >
         Get Activity
       </button>
-      {!!current && <ActivityCard />}
+      {!current && <p>No Activities, click the button above</p>}
+      {!!current && <CurrentActivityCard />}
+      {!previous.length && <p>No previous activities, consider clicking the button above</p>}
+      {!!previous.length && <PreviousActivities />}
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return { ...state };
-};
+const mapStateToProps = state => (state)
 
 export default connect(mapStateToProps, { getActivity })(App);
